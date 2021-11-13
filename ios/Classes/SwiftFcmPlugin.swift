dc6db5fb557a81c2dd8766481a6d91b3f7ee1a25
abdulrahman tarek <31620727+abdelrahmantarek@users.noreply.github.com>
2021-11-13 02:00:59 +0200

    fix ios run only

--- ios/Classes/SwiftFcmPlugin.swift
+++ ios/Classes/SwiftFcmPlugin.swift
@@ -1,7 +1,7 @@
 import Flutter
 import UIKit
 import FirebaseMessaging
-
+import FirebaseCore
 
 
 
@@ -9,20 +9,83 @@ public class SwiftFcmPlugin: NSObject, FlutterPlugin,MessagingDelegate {
     
     
     
+  var channel: FlutterMethodChannel?
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "fcm", binaryMessenger: registrar.messenger())
     let instance = SwiftFcmPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
-      
+    registrar.addApplicationDelegate(instance)
+      print("start register --------------------------------------------- 1")
   }
+
     
-  public func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingRemoteMessage) {
+
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+        print("start application --------------------------------------------- 2")
+        
+        
+        FirebaseApp.configure()
+
+          // [START set_messaging_delegate]
+          Messaging.messaging().delegate = self
+          // [END set_messaging_delegate]
+
+
+
+    //      Messaging.messaging().fcmToken { fcmToken, error in
+    //        if let error = error {
+    //          print("Error fetching FCM registration token: \(error)")
+    //        } else if let token = token {
+    //          print("FCM registration token: \(token)")
+    //          self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)"
+    //        }
+    //      }
+
+
+
+
+          // Register for remote notifications. This shows a permission dialog on first run, to
+          // show the dialog at a more appropriate time move this registration accordingly.
+          // [START register_for_notifications]
+          if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+              options: authOptions,
+              completionHandler: { _, _ in }
+            )
+          } else {
+            let settings: UIUserNotificationSettings =
+              UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+              application.registerUserNotificationSettings(settings)
+          }
+
+          application.registerForRemoteNotifications()
+        
+
+        // [END register_for_notifications]
+        
+        
+        return true
+    }
+    
+
+    
+
+    
+    
+    
+  public func messaging(_ messaging: Messaging, didReceive remoteMessage: Any) {
 
  
+
   }
 
     
-  public func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+  public func messaging( didReceiveRegistrationToken fcmToken: Any) {
       print("Firebase registration token: \(String(describing: fcmToken))")
 //      let dataDict: [String: String] = ["token": fcmToken ?? ""]
 //      NotificationCenter.default.post(
@@ -33,63 +96,17 @@ public class SwiftFcmPlugin: NSObject, FlutterPlugin,MessagingDelegate {
       // TODO: If necessary send token to application server.
       // Note: This callback is fired at each app startup and whenever a new token is generated.
   }
-    
-    
-    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("start application --------------------------------------------- ")
-//
-        FirebaseApp.configure()
-//
-//        // [START set_messaging_delegate]
-//        Messaging.messaging().delegate = self
-//        // [END set_messaging_delegate]
-//
-//
-//
-//  //      Messaging.messaging().fcmToken { fcmToken, error in
-//  //        if let error = error {
-//  //          print("Error fetching FCM registration token: \(error)")
-//  //        } else if let token = token {
-//  //          print("FCM registration token: \(token)")
-//  //          self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)"
-//  //        }
-//  //      }
-//
-//
-//
-//
-//        // Register for remote notifications. This shows a permission dialog on first run, to
-//        // show the dialog at a more appropriate time move this registration accordingly.
-//        // [START register_for_notifications]
-//        if #available(iOS 10.0, *) {
-//          // For iOS 10 display notification (sent via APNS)
-//          UNUserNotificationCenter.current().delegate = self
-//
-//          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-//          UNUserNotificationCenter.current().requestAuthorization(
-//            options: authOptions,
-//            completionHandler: { _, _ in }
-//          )
-//        } else {
-//          let settings: UIUserNotificationSettings =
-//            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-//            application.registerUserNotificationSettings(settings)
-//        }
-//
-//        application.registerForRemoteNotifications()
-        
-
-        // [END register_for_notifications]
-        
-    }
+   
 
 
 
-    
-    
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+    
+    
+    
+    
     
 }
 
--- ios/fcm.podspec
+++ ios/fcm.podspec
@@ -21,7 +21,7 @@ A new Flutter project.
   s.platform = :ios, '8.0'
   
   s.dependency 'Firebase'
-  s.dependency 'Firebase/Messaging', "~> 6.0.0"
+  s.dependency 'Firebase/Messaging'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
