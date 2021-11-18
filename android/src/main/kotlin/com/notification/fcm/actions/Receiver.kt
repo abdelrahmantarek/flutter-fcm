480250c1708ea0dca943c409baa21ba18bb4b458
AbdulrahmanGmail <92045686+AbdulrahmanGmail@users.noreply.github.com>
2021-11-18 18:53:22 +0200

    Fix Receiver Android

--- android/src/main/kotlin/com/notification/fcm/actions/Receiver.kt
+++ android/src/main/kotlin/com/notification/fcm/actions/Receiver.kt
@@ -13,7 +13,6 @@ class Receiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, receiver: Intent?) {
 
         if(receiver?.action == null){
-            Log.d(TAG, "Receiver : ${receiver?.action}")
             val sharedPreference = context?.getSharedPreferences(OnMessageReceived.NOTIFICATION,Context.MODE_PRIVATE)
             val intent = Intent().setClassName(context!!,context.packageName+".MainActivity")
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
