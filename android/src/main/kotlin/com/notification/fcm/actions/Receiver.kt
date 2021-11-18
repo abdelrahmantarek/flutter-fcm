8eb8e36eb8dea954064491ab4cd015ed2304fa00
AbdulrahmanGmail <92045686+AbdulrahmanGmail@users.noreply.github.com>
2021-11-18 18:44:36 +0200

    Fix Receiver Android

--- android/src/main/kotlin/com/notification/fcm/actions/Receiver.kt
+++ android/src/main/kotlin/com/notification/fcm/actions/Receiver.kt
@@ -11,13 +11,19 @@ class Receiver : BroadcastReceiver() {
 
     @SuppressLint("LongLogTag")
     override fun onReceive(context: Context?, receiver: Intent?) {
-        val sharedPreference = context?.getSharedPreferences(OnMessageReceived.NOTIFICATION,Context.MODE_PRIVATE)
-        val intent = Intent().setClassName(context!!,context.packageName+".MainActivity")
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        intent.action = receiver?.action
-        intent.putExtra("data",sharedPreference?.getString("data","null"))
-        context.startActivity(intent)
+
+        if(receiver?.action == null){
+            Log.d(TAG, "Receiver : ${receiver?.action}")
+            val sharedPreference = context?.getSharedPreferences(OnMessageReceived.NOTIFICATION,Context.MODE_PRIVATE)
+            val intent = Intent().setClassName(context!!,context.packageName+".MainActivity")
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            intent.action = receiver?.action
+            intent.putExtra("data",sharedPreference?.getString("data","null"))
+            context.startActivity(intent)
+
+        }
+
     }
 
 }
